e184257b663b8116128b691ef153cdf44fbdb0e4
Ahmet Kaytan <ahmetkaytan3599@gmai.com>
2024-11-18 19:00:17 +0300

    CategoryService added

--- src/app/product-create/product-create.component.ts
+++ src/app/product-create/product-create.component.ts
@@ -1,18 +1,29 @@
 import { Component } from '@angular/core';
 import { ProductService } from '../services/product.service';
 import { Router } from '@angular/router';
+import { CategoryService } from '../services/category.service';
+import { Category } from '../models/category';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-product-create',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './product-create.component.html',
-  styleUrl: './product-create.component.css'
+  styleUrl: './product-create.component.css',
+  providers:[CategoryService]
 })
 export class ProductCreateComponent {
 
-  constructor(private productService: ProductService, private router:Router){
+  categories:Category[] = [];
+  constructor(private productService: ProductService, private categoryService:CategoryService, private router:Router){
 
+  }
+
+  ngOnInit():void{
+    this.categoryService.getCategories().subscribe(data =>{
+      this.categories = data;
+    });
   }
 
   saveProduct(name:any,price:any,imageUrl:any,description:any,isActive:any,categodyId:any){
